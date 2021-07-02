a197ab1735ac7e423eff541316b488751688f832
minijaham <60860894+minijaham@users.noreply.github.com>
2021-07-02 18:42:15 +0900

    Update LazerEnchant.php

--- src/DaPigGuy/PiggyCustomEnchants/enchants/tools/pickaxe/LazerEnchant.php
+++ src/DaPigGuy/PiggyCustomEnchants/enchants/tools/pickaxe/LazerEnchant.php
@@ -30,7 +30,27 @@ class LazerEnchant extends ReactiveEnchantment
     public function react(Player $player, Item $item, Inventory $inventory, int $slot, Event $event, int $level, int $stack): void
     {
         if ($event instanceof PlayerInteractEvent) {
-          
+          $nbt = new CompoundTag('', [
+		    'Pos' => new ListTag('Pos', [
+			    new DoubleTag('', $player->getX()),
+		    	new DoubleTag('', $player->getY() + $player->getEyeHeight()),
+		    	new DoubleTag('', $player->getZ())
+		    ]),
+		    'Motion' => new ListTag('Motion', [
+		    	new DoubleTag("", -sin($player->yaw / 180 * M_PI) * cos($player->pitch / 180 * M_PI)),
+		    	new DoubleTag("", -sin($player->pitch / 180 * M_PI)),
+			    new DoubleTag("", cos($player->yaw / 180 * M_PI) * cos($player->pitch / 180 * M_PI))
+	    	]),
+		    'Rotation' => new ListTag('Rotation', [
+		    	new FloatTag('', lcg_value() * 360),
+		    	new FloatTag('', 0)
+	    	]),
+	    ]);
+	    $entity = Entity::createEntity("PiggySnowball", $player->getLevel(), $nbt);
+	    $entity->setOwningEntity($player);
+	    $entity->setMotion($entity->getMotion()->multiply(2));
+	    $entity->setCanSaveWithChunk(false);
+	    $entity->spawnToAll();
         }
     }
 }
